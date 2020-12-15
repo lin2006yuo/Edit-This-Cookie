b29b0f7e84690ec26cf2da0263ddf12c8da962e8
linxueyu <lin2006yuo@gmail.com>
2020-12-15 23:04:09 +0800

    feat: m

--- src/popup/App/App.vue
+++ src/popup/App/App.vue
@@ -1,25 +1,38 @@
 <template>
-  <div class="main_app">
-    <h5>dom选择器</h5>
+  <div class="main_app" style="width: 200px">
+    <!-- <h5>dom选择器</h5>
     <input v-model="inputValue" />
-    <button @click="handleClick" class="button button-outline">搜索</button>
+    <button @click="handleClick" class="button button-outline">搜索</button> -->
+    <fieldset>
+      <label for="nameField">dom选择器</label>
+      <input
+        v-model="inputValue"
+        type="text"
+        placeholder="输入要监听的dom元素"
+        id="nameField"
+      />
+      <button @click="handleClick" class="button-primary">
+        搜索
+      </button>
+    </fieldset>
+    <span href="">debugger</span>
+    <div>
+      <button
+        @click="handleInsert"
+        class="button-primary"
+        type="submit"
+        value="插入元素"
+      />
+    </div>
   </div>
 </template>
 
 <script>
-import qs from "query-string"
-import { filterByKeys, getOptions } from "./util"
-import Input from "../../components/input"
-import Button from "../../components/button"
-import Select from "../../components/select"
-import PannelItem from "../../components/pannel-item"
-
-const GM_COOKIES_KEY = ["cms_key", "cshop_cms_key", "sessionid", "group_id"]
 export default {
   name: "app",
   data() {
     return {
-      inputValue: ".",
+      inputValue: "body"
     }
   },
   computed: {},
@@ -33,15 +46,25 @@ export default {
         alert("选择器不合法")
         return
       }
-      console.log(dom)
-      if (!dom) {
-        alert("找不到元素")
-      }
+      this.sendMessageToContentScript(
+        {
+          action: "bindDom",
+          value: this.inputValue
+        },
+        (res) => {
+          console.log(res.dom)
+        }
+      )
+    },
+    handleInsert() {
+      this.sendMessageToContentScript({
+        action: 'insert'
+      })
     },
     sendMessageToContentScript(message, callback) {
       this.getCurrentTabId((tabId) => {
         chrome.tabs.sendMessage(tabId, message, function(response) {
-          if (callback) callback(response)
+          if (callback && response) callback(response)
         })
       })
     },
@@ -49,14 +72,9 @@ export default {
       chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
         if (callback) callback(tabs.length ? tabs[0].id : null)
       })
-    },
+    }
   },
-  components: {
-    Input,
-    Button,
-    Select,
-    PannelItem,
-  },
+  components: {}
 }
 </script>
 
--- src/script/content-script.js
+++ src/script/content-script.js
@@ -1,3 +1,5 @@
+console.log("hello content-script")
+let dom
 document.addEventListener("DOMContentLoaded", () => {
   let dom = document.querySelector("#some-id")
   if (dom) {
@@ -27,5 +29,56 @@ document.addEventListener("DOMContentLoaded", () => {
 
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
   // console.log(sender.tab ?"from a content script:" + sender.tab.url :"from the extension");
-  sendResponse("我收到了你的消息")
+  switch (request.action) {
+    case "bindDom":
+      dom = document.querySelector(request.value)
+      if (!dom) sendResponse({ error: true, msg: "找不到元素" })
+      else {
+        // 监听dom
+        const callback = function(mutationsList) {
+          for (let mutation of mutationsList) {
+            if (mutation.type === "childList") {
+              console.log(chrome)
+              console.log("A child node has been added or removed.")
+
+              chrome.runtime.sendMessage(
+                {
+                  greeting: "你好，我是content-script呀，我主动发消息给后台！"
+                },
+                function(response) {
+                  console.log("收到来自后台的回复：" + response)
+                }
+              )
+            } else if (mutation.type === "attributes") {
+              console.log(
+                "The " + mutation.attributeName + " attribute was modified."
+              )
+            }
+          }
+        }
+        const observer = new MutationObserver(callback)
+        observer.observe(dom, { childList: true })
+
+        sendResponse({
+          error: false,
+          msh: "",
+          payload: {
+            dom
+          }
+        })
+      }
+      break
+    case "insert":
+      if(dom) {
+        var node=document.createElement("LI");
+        var textnode=document.createTextNode("Water");
+        node.appendChild(textnode);
+        dom.appendChild(node);
+      }
+      break
+    default:
+      break
+  }
+  if (request.action === "bindDom") {
+  }
 })
